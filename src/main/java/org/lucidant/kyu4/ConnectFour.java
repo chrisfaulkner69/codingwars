9d741ac7d4f7ceb35735be1da1ba5c641a30c0f7
Chris Faulkner <chris.faulkner@rs-components.com>
2019-01-24 20:55:38 +0000

    Added empty class and intellij updates for .gitignore

--- src/main/java/org/lucidant/kyu4/ConnectFour.java
+++ src/main/java/org/lucidant/kyu4/ConnectFour.java
@@ -5,52 +5,46 @@ import java.util.List;
 
 import static org.lucidant.kyu4.WinStatus.DRAW;
 
+// https://www.codewars.com/kata/connect-four-1
 public class ConnectFour {
 
+    private static final String SEPARATOR = "_";
     private static final int ROW_COUNT = 6;
     static final List<String> COL_NAMES = Arrays.asList("A", "B", "C", "D", "E", "F", "G");
     private static final int COL_COUNT = COL_NAMES.size();
 
-    static int COL_A = COL_NAMES.indexOf("A");
-    static int COL_B = COL_NAMES.indexOf("B");
-    static int COL_C = COL_NAMES.indexOf("C");
-    static int COL_D = COL_NAMES.indexOf("D");
-    static int COL_E = COL_NAMES.indexOf("E");
-    static int COL_F = COL_NAMES.indexOf("F");
-    static int COL_G = COL_NAMES.indexOf("G");
-
     public static String whoIsWinner(final List<String> myList) {
 
         // Not enough moves to decide a winner
-        if (myList.isEmpty() || myList == null || myList.size() < 7) {
+        if (myList == null || myList.isEmpty() || myList.size() < 7) {
             return DRAW.toString();
         }
 
         //
-        return storeMoves(myList, ROW_COUNT, COL_COUNT).toString();
+        return whoWins(myList).toString();
     }
 
+    private static WinStatus whoWins(final List<String> myList) {
 
-    static WinStatus storeMoves(final List<String> myList, final int cols, final int rows) {
-
-        final WinStatus[][] board = new WinStatus[cols][rows];
+        final WinStatus[][] board = new WinStatus[ROW_COUNT][COL_COUNT];
          // We have A_Red, B_Yellow as the identifiers
-        for (String move : myList) {
-            String[] res = move.split("_");
+        for (final String move : myList) {
+            final String[] res = move.split(SEPARATOR);
             final WinStatus counter =  WinStatus.valueOf(res[1].toUpperCase());
-            if (addToBoard(board, res[0], counter)) {
+            final String colName = res[0];
+            if (addAndCheckForWinner(board, colName.toUpperCase(), counter)) {
                 return counter;
             }
         }
         return WinStatus.DRAW;
     }
 
-    private static boolean addToBoard(WinStatus[][] board, String colName, WinStatus piece) {
+    private static boolean addAndCheckForWinner(final WinStatus[][] board, final String colName, final WinStatus piece) {
 
         final int colNum = COL_NAMES.indexOf(colName);
 
         // Looking for the first empty row for the column, then add
-        for (int row = 0 ; row< ROW_COUNT ; row++) {
+        for (int row = 0 ; row < ROW_COUNT ; row++) {
             if (board[row][colNum] == null) {
                 System.out.println("Added " + piece.toString() + " to row : " + row + " and col :" + colNum);
                 board[row][colNum] = piece;
@@ -60,21 +54,19 @@ public class ConnectFour {
         return false;
     }
 
-    private static boolean checkWinner(WinStatus[][] board, int row, int colNum, WinStatus piece) {
-
+    private static boolean checkWinner(final WinStatus[][] board, final int row, final int colNum, final WinStatus piece) {
 
         // Row to the right
-        int targetColToRight = colNum + 3;
-        int targetColToLeft = colNum - 3;
-        if (checkRow(board, targetColToLeft, colNum, row, piece)) {
-            return true;
-        }
-        if (checkRow(board, colNum, targetColToRight, row, piece)) {
+        if (checkRow(board, colNum, row, piece)) {
             return true;
         }
 
-        // column check
-        int targetRowBelow = row - 3;
+        if (checkDiagonal(board, colNum, row, piece)) {
+            return true;
+        }
+
+        // column check - only need to check down
+        final int targetRowBelow = row - 3;
         if (targetRowBelow < 0) {
             return false;
         }
@@ -87,18 +79,32 @@ public class ConnectFour {
 
     }
 
-    private static boolean checkRow(WinStatus[][] board, int leftCol, int rightCol, int row, WinStatus piece) {
+    private static boolean checkDiagonal(WinStatus[][] board, int colNum, int row, WinStatus piece) {
+        int consecutiveNorWstSthEasr = 0;
+        int consecutiveNorEastSthWest = 0;
 
-        if (leftCol < 0 || rightCol > (COL_COUNT - 1)) {
-            return false;
-        }
 
-        for (int colNum = leftCol ; colNum <= rightCol ; colNum++) {
+    }
+
+    private static boolean checkRow(final WinStatus[][] board, final int startCol, final int row, final WinStatus piece) {
+
+        int consecutiveToRight = 0;
+        int consecutiveToLeft = 0;
+        for (int colNum = startCol + 1 ; colNum <= COL_COUNT - 1 ; colNum++) {
             if (board[row][colNum] != piece) {
-                return false;
+                break;
             }
+            consecutiveToRight++;
         }
-        return true;
+
+        for (int colNum = startCol - 1; colNum >= 0 ; colNum--) {
+            if (board[row][colNum] != piece) {
+                break;
+            }
+            consecutiveToLeft++;
+        }
+
+        return  (consecutiveToRight + consecutiveToLeft + 1) >= 4;
     }
 
 }
--- src/test/java/org/lucidant/kyu4/ConnectFourTest.java
+++ src/test/java/org/lucidant/kyu4/ConnectFourTest.java
@@ -1,6 +1,7 @@
 package org.lucidant.kyu4;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,21 +10,18 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.lucidant.kyu4.ConnectFour.*;
 
 class ConnectFourTest {
 
     @DisplayName("Nobody has a go, it's a draw")
     @Test
-    public void testNoGoes()
-    {
-        Assertions.assertEquals( "Draw", ConnectFour.whoIsWinner(Collections.<String>emptyList()));
+    void testNoGoes() {
+        Assertions.assertEquals("Draw", ConnectFour.whoIsWinner(Collections.emptyList()));
     }
 
     @DisplayName("Yellow gets a win by a column in B")
     @Test
-    public void yellowUnInterruptedInColB()
-    {
+    void yellowUnInterruptedInColB() {
         final List<String> myList = Arrays.asList(
                 "A_Red",
                 "B_Yellow",
@@ -39,8 +37,7 @@ class ConnectFourTest {
 
     @DisplayName("Red wins on row 0, between C and F sequentially")
     @Test
-    public void redOnRowAYellowCopyingAbove()
-    {
+    void redOnRowAYellowCopyingAbove() {
         final List<String> myList = Arrays.asList(
                 "C_Red",
                 "C_Yellow",
@@ -55,17 +52,61 @@ class ConnectFourTest {
 
     @DisplayName("Red wins on row 0, between C and F but does C then F then E then D")
     @Test
-    public void redOnRowAYellowCopyingAboveButRedNotSequential()
-    {
+    void redOnRowAYellowCopyingAboveButRedNotSequential() {
         final List<String> myList = Arrays.asList(
                 "C_Red",
-                "C_Yellow",
+                "A_Yellow",
                 "F_Red",
-                "D_Yellow",
+                "G_Yellow",
                 "E_Red",
-                "E_Yellow",
+                "G_Yellow",
                 "D_Red"
         );
         assertEquals("Red", ConnectFour.whoIsWinner(myList));
+    }
+
+    @DisplayName("Red wins on row 0, does so ending in G.")
+    @Test
+    void redOnRowEndInG() {
+        final List<String> myList = Arrays.asList(
+                "D_Red",
+                "C_Yellow",
+                "E_Red",
+                "D_Yellow",
+                "G_Red",
+                "E_Yellow",
+                "F_Red"
+        );
+        assertEquals("Red", ConnectFour.whoIsWinner(myList));
+    }
+
+    @DisplayName("Yellow wins with a diagonal to north west. A0, B1, C2 and D3.")
+    @Test
+    void boardDiagonalYellowFromOrigin() {
+        final List<String> myList = Arrays.asList(
+                "A_Yellow", "B_Red", "B_Yellow", "C_Red",
+                "G_Yellow", "C_Red", "C_Yellow", "D_Red",
+                "E_Yellow", "D_Red", "D_Yellow", "G_Red",
+                "D_Yellow"
+        );
+        assertEquals("Yellow", ConnectFour.whoIsWinner(myList));
+    }
+
+    @Disabled
+    @DisplayName("Board is full with no winner.")
+    @Test
+    void boardFullNoWinner() {
+        final List<String> myList = Arrays.asList(
+                "A_Red", "G_Yellow", "B_Red", "F_Yellow", "E_Red", "C_Yellow", "D_Red",
+                "A_Yellow", "G_Red", "B_Yellow", "F_Red", "G_Yellow", "A_Red",
+                "D_Yellow", "C_Red", "E_Yellow", "D_Red", "E_Yellow", "G_Red",
+
+                "C_Yellow", "F_Red", "B_Yellow", "F_Red", "E_Yellow", "A_Red",
+
+                "F_Yellow", "B_Red", "D_Yellow", "F_Red", "A_Yellow", "A_Red",
+
+                "A_Yellow", "B_Red", "C_Yellow", "D_Red", "E_Yellow", "D_Red"
+        );
+        assertEquals("Draw", ConnectFour.whoIsWinner(myList));
     }
 }
