9d567dbfeb495511c08b016462c9764eb859eff1
christweekie <chris.faulkner@gmail.com>
2023-12-05 08:03:44 +0000

    Added WhatsInAName

---
+++ src/main/java/org/lucidant/kyu6/WhatsInAName.java
@@ -0,0 +1,13 @@
+package org.lucidant.kyu6;
+
+/**
+ * String intersection.
+ * https://www.codewars.com/kata/59daf400beec9780a9000045
+ */
+public class WhatsInAName {
+
+    public static boolean nameInStr(final String source, final String name){
+        return false;
+    }
+
+}
---
+++ src/test/java/org/lucidant/kyu6/WhatsInANameTest.java
@@ -0,0 +1,7 @@
+package org.lucidant.kyu6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WhatsInANameTest {
+
+}
