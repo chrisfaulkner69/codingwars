5d2e874d3b7e10429a2224dcfcdb9c50df0a0a98
Chris Faulkner <chris.faulkner@digital.homeoffice.gov.uk>
2023-12-14 21:04:14 +0000

    Cuckoo Clck

---
+++ src/main/java/org/lucidant/kyu5/PigLatin.java
@@ -0,0 +1,31 @@
+package org.lucidant.kyu5;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class PigLatin {
+    private static final Pattern regex = Pattern.compile("(\\w)(\\w*)");
+    private static final Pattern WORD = Pattern.compile("^([A-Za-z0-9]{1,})$");
+    private static final String APPEND = "ay";
+
+    public static String pigItFromWeb(String str) {
+        return regex.matcher(str).replaceAll("$2$1ay");
+    }
+    public static String pigIt(String str) {
+        return Arrays.stream(str.split("\\s"))
+                .map(PigLatin::pigWord)
+                .collect(Collectors.joining(" "));
+
+    }
+    private static String pigWord(final String str) {
+        if (!WORD.matcher(str).matches()) {
+            return str;
+        }
+        return str.length() > 1 ?
+                str.substring(1) + str.charAt(0) + APPEND
+                : str + APPEND;
+
+    }
+
+}
