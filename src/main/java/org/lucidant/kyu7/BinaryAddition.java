ed9eeddb8c38edaff2687b6b931d35f5c8b30214
christweekie <chris.faulkner@gmail.com>
2024-05-07 15:15:37 +0100

    BinaryAddition

--- src/main/java/org/lucidant/kyu7/BinaryAddition.java
+++ src/main/java/org/lucidant/kyu7/BinaryAddition.java
@@ -1,5 +1,10 @@
 package org.lucidant.kyu7;
 
+/**
+ * Binary Addition.
+ * @see <a href="https://www.codewars.com/kata/551f37452ff852b7bd000139">Source</a>
+ *
+ */
 public class BinaryAddition {
 
     static String binaryAddition(int a, int b){
